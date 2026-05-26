22f5bc1d3149afa343dc20f3cc817ff87f6d2b81
Arthur <Arthur@DESKTOP-JJJAAI3>
2026-05-26 19:35:41 -0300

    Adicionar arquivos de projeto.

---
+++ AcademiaSaude/FrmPerfil.Designer.cs
@@ -0,0 +1,454 @@
+﻿namespace AcademiaSaude
+{
+	partial class FrmPerfil
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.pictureBox7 = new System.Windows.Forms.PictureBox();
+			this.pictureBox8 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox9 = new System.Windows.Forms.PictureBox();
+			this.picturePerf = new System.Windows.Forms.PictureBox();
+			this.lblEstr1 = new System.Windows.Forms.Label();
+			this.lblEstr2 = new System.Windows.Forms.Label();
+			this.lblEstr3 = new System.Windows.Forms.Label();
+			this.lblEstr4 = new System.Windows.Forms.Label();
+			this.lblEstr5 = new System.Windows.Forms.Label();
+			this.lblNm = new System.Windows.Forms.Label();
+			this.txtNome = new System.Windows.Forms.TextBox();
+			this.lblSbn = new System.Windows.Forms.Label();
+			this.txtSobr = new System.Windows.Forms.TextBox();
+			this.lblIdd = new System.Windows.Forms.Label();
+			this.txtIdd = new System.Windows.Forms.TextBox();
+			this.pictureBox10 = new System.Windows.Forms.PictureBox();
+			this.lblEmail = new System.Windows.Forms.Label();
+			this.txtEml = new System.Windows.Forms.TextBox();
+			this.lblSenha = new System.Windows.Forms.Label();
+			this.txtSen = new System.Windows.Forms.TextBox();
+			this.lblAval = new System.Windows.Forms.Label();
+			this.btnMostrar = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picturePerf)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Red;
+			this.pictureBox1.Location = new System.Drawing.Point(1, 57);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 10);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Black;
+			this.pictureBox3.Location = new System.Drawing.Point(471, 0);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(330, 59);
+			this.pictureBox3.TabIndex = 2;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BackColor = System.Drawing.Color.Black;
+			this.pictureBox4.Location = new System.Drawing.Point(1, 0);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(303, 59);
+			this.pictureBox4.TabIndex = 3;
+			this.pictureBox4.TabStop = false;
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.BackColor = System.Drawing.Color.Black;
+			this.pictureBox5.Image = global::AcademiaSaude.Properties.Resources.Logotipo_Academia_Moderno_Vermelho_e_preto__1_;
+			this.pictureBox5.Location = new System.Drawing.Point(301, 0);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(174, 59);
+			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox5.TabIndex = 5;
+			this.pictureBox5.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Gainsboro;
+			this.pictureBox2.Location = new System.Drawing.Point(45, 73);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(713, 365);
+			this.pictureBox2.TabIndex = 6;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.BackColor = System.Drawing.Color.Red;
+			this.pictureBox6.Location = new System.Drawing.Point(45, 73);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(713, 65);
+			this.pictureBox6.TabIndex = 7;
+			this.pictureBox6.TabStop = false;
+			this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+			// 
+			// pictureBox7
+			// 
+			this.pictureBox7.BackColor = System.Drawing.Color.White;
+			this.pictureBox7.Location = new System.Drawing.Point(73, 85);
+			this.pictureBox7.Name = "pictureBox7";
+			this.pictureBox7.Size = new System.Drawing.Size(277, 333);
+			this.pictureBox7.TabIndex = 8;
+			this.pictureBox7.TabStop = false;
+			// 
+			// pictureBox8
+			// 
+			this.pictureBox8.BackColor = System.Drawing.Color.White;
+			this.pictureBox8.Location = new System.Drawing.Point(439, 85);
+			this.pictureBox8.Name = "pictureBox8";
+			this.pictureBox8.Size = new System.Drawing.Size(277, 333);
+			this.pictureBox8.TabIndex = 9;
+			this.pictureBox8.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.White;
+			this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Black;
+			this.label1.Location = new System.Drawing.Point(157, 94);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(107, 19);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "MEU PERFIL";
+			// 
+			// pictureBox9
+			// 
+			this.pictureBox9.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.pictureBox9.Location = new System.Drawing.Point(87, 178);
+			this.pictureBox9.Name = "pictureBox9";
+			this.pictureBox9.Size = new System.Drawing.Size(217, 231);
+			this.pictureBox9.TabIndex = 11;
+			this.pictureBox9.TabStop = false;
+			// 
+			// picturePerf
+			// 
+			this.picturePerf.BackColor = System.Drawing.Color.White;
+			this.picturePerf.Image = global::AcademiaSaude.Properties.Resources.user;
+			this.picturePerf.Location = new System.Drawing.Point(87, 116);
+			this.picturePerf.Name = "picturePerf";
+			this.picturePerf.Size = new System.Drawing.Size(59, 56);
+			this.picturePerf.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.picturePerf.TabIndex = 12;
+			this.picturePerf.TabStop = false;
+			// 
+			// lblEstr1
+			// 
+			this.lblEstr1.AutoSize = true;
+			this.lblEstr1.BackColor = System.Drawing.Color.White;
+			this.lblEstr1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblEstr1.ForeColor = System.Drawing.Color.Gold;
+			this.lblEstr1.Location = new System.Drawing.Point(514, 368);
+			this.lblEstr1.Name = "lblEstr1";
+			this.lblEstr1.Size = new System.Drawing.Size(32, 31);
+			this.lblEstr1.TabIndex = 13;
+			this.lblEstr1.Text = "★";
+			this.lblEstr1.Click += new System.EventHandler(this.llbEstr1_Click);
+			// 
+			// lblEstr2
+			// 
+			this.lblEstr2.AutoSize = true;
+			this.lblEstr2.BackColor = System.Drawing.Color.White;
+			this.lblEstr2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblEstr2.ForeColor = System.Drawing.Color.Gold;
+			this.lblEstr2.Location = new System.Drawing.Point(539, 368);
+			this.lblEstr2.Name = "lblEstr2";
+			this.lblEstr2.Size = new System.Drawing.Size(32, 31);
+			this.lblEstr2.TabIndex = 14;
+			this.lblEstr2.Text = "★";
+			this.lblEstr2.Click += new System.EventHandler(this.lblEstr2_Click);
+			// 
+			// lblEstr3
+			// 
+			this.lblEstr3.AutoSize = true;
+			this.lblEstr3.BackColor = System.Drawing.Color.White;
+			this.lblEstr3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblEstr3.ForeColor = System.Drawing.Color.Gold;
+			this.lblEstr3.Location = new System.Drawing.Point(563, 368);
+			this.lblEstr3.Name = "lblEstr3";
+			this.lblEstr3.Size = new System.Drawing.Size(32, 31);
+			this.lblEstr3.TabIndex = 15;
+			this.lblEstr3.Text = "★";
+			this.lblEstr3.Click += new System.EventHandler(this.lblEstr3_Click);
+			// 
+			// lblEstr4
+			// 
+			this.lblEstr4.AutoSize = true;
+			this.lblEstr4.BackColor = System.Drawing.Color.White;
+			this.lblEstr4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblEstr4.ForeColor = System.Drawing.Color.Gold;
+			this.lblEstr4.Location = new System.Drawing.Point(589, 368);
+			this.lblEstr4.Name = "lblEstr4";
+			this.lblEstr4.Size = new System.Drawing.Size(32, 31);
+			this.lblEstr4.TabIndex = 16;
+			this.lblEstr4.Text = "★";
+			this.lblEstr4.Click += new System.EventHandler(this.lblEstr4_Click);
+			// 
+			// lblEstr5
+			// 
+			this.lblEstr5.AutoSize = true;
+			this.lblEstr5.BackColor = System.Drawing.Color.White;
+			this.lblEstr5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblEstr5.ForeColor = System.Drawing.Color.Gold;
+			this.lblEstr5.Location = new System.Drawing.Point(618, 368);
+			this.lblEstr5.Name = "lblEstr5";
+			this.lblEstr5.Size = new System.Drawing.Size(32, 31);
+			this.lblEstr5.TabIndex = 17;
+			this.lblEstr5.Text = "★\r\n";
+			this.lblEstr5.Click += new System.EventHandler(this.lblEstr5_Click);
+			// 
+			// lblNm
+			// 
+			this.lblNm.AutoSize = true;
+			this.lblNm.ForeColor = System.Drawing.Color.Black;
+			this.lblNm.Location = new System.Drawing.Point(94, 194);
+			this.lblNm.Name = "lblNm";
+			this.lblNm.Size = new System.Drawing.Size(57, 13);
+			this.lblNm.TabIndex = 18;
+			this.lblNm.Text = "Seu Nome";
+			// 
+			// txtNome
+			// 
+			this.txtNome.Location = new System.Drawing.Point(97, 219);
+			this.txtNome.Name = "txtNome";
+			this.txtNome.ReadOnly = true;
+			this.txtNome.Size = new System.Drawing.Size(156, 20);
+			this.txtNome.TabIndex = 19;
+			this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+			// 
+			// lblSbn
+			// 
+			this.lblSbn.AutoSize = true;
+			this.lblSbn.ForeColor = System.Drawing.Color.Black;
+			this.lblSbn.Location = new System.Drawing.Point(94, 255);
+			this.lblSbn.Name = "lblSbn";
+			this.lblSbn.Size = new System.Drawing.Size(61, 13);
+			this.lblSbn.TabIndex = 20;
+			this.lblSbn.Text = "Sobrenome";
+			// 
+			// txtSobr
+			// 
+			this.txtSobr.Location = new System.Drawing.Point(97, 287);
+			this.txtSobr.Name = "txtSobr";
+			this.txtSobr.ReadOnly = true;
+			this.txtSobr.Size = new System.Drawing.Size(156, 20);
+			this.txtSobr.TabIndex = 21;
+			// 
+			// lblIdd
+			// 
+			this.lblIdd.AutoSize = true;
+			this.lblIdd.Location = new System.Drawing.Point(94, 331);
+			this.lblIdd.Name = "lblIdd";
+			this.lblIdd.Size = new System.Drawing.Size(34, 13);
+			this.lblIdd.TabIndex = 22;
+			this.lblIdd.Text = "Idade";
+			// 
+			// txtIdd
+			// 
+			this.txtIdd.Location = new System.Drawing.Point(97, 358);
+			this.txtIdd.Name = "txtIdd";
+			this.txtIdd.ReadOnly = true;
+			this.txtIdd.Size = new System.Drawing.Size(156, 20);
+			this.txtIdd.TabIndex = 23;
+			// 
+			// pictureBox10
+			// 
+			this.pictureBox10.BackColor = System.Drawing.Color.WhiteSmoke;
+			this.pictureBox10.Location = new System.Drawing.Point(456, 104);
+			this.pictureBox10.Name = "pictureBox10";
+			this.pictureBox10.Size = new System.Drawing.Size(219, 203);
+			this.pictureBox10.TabIndex = 24;
+			this.pictureBox10.TabStop = false;
+			// 
+			// lblEmail
+			// 
+			this.lblEmail.AutoSize = true;
+			this.lblEmail.Location = new System.Drawing.Point(468, 125);
+			this.lblEmail.Name = "lblEmail";
+			this.lblEmail.Size = new System.Drawing.Size(32, 13);
+			this.lblEmail.TabIndex = 25;
+			this.lblEmail.Text = "Email";
+			// 
+			// txtEml
+			// 
+			this.txtEml.Location = new System.Drawing.Point(471, 152);
+			this.txtEml.Name = "txtEml";
+			this.txtEml.ReadOnly = true;
+			this.txtEml.Size = new System.Drawing.Size(157, 20);
+			this.txtEml.TabIndex = 26;
+			// 
+			// lblSenha
+			// 
+			this.lblSenha.AutoSize = true;
+			this.lblSenha.Location = new System.Drawing.Point(472, 217);
+			this.lblSenha.Name = "lblSenha";
+			this.lblSenha.Size = new System.Drawing.Size(38, 13);
+			this.lblSenha.TabIndex = 27;
+			this.lblSenha.Text = "Senha";
+			// 
+			// txtSen
+			// 
+			this.txtSen.Location = new System.Drawing.Point(471, 248);
+			this.txtSen.Name = "txtSen";
+			this.txtSen.ReadOnly = true;
+			this.txtSen.Size = new System.Drawing.Size(157, 20);
+			this.txtSen.TabIndex = 28;
+			this.txtSen.UseSystemPasswordChar = true;
+			// 
+			// lblAval
+			// 
+			this.lblAval.AutoSize = true;
+			this.lblAval.BackColor = System.Drawing.Color.White;
+			this.lblAval.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblAval.Location = new System.Drawing.Point(542, 353);
+			this.lblAval.Name = "lblAval";
+			this.lblAval.Size = new System.Drawing.Size(64, 15);
+			this.lblAval.TabIndex = 29;
+			this.lblAval.Text = "Nos Avalie";
+			// 
+			// btnMostrar
+			// 
+			this.btnMostrar.Location = new System.Drawing.Point(634, 248);
+			this.btnMostrar.Name = "btnMostrar";
+			this.btnMostrar.Size = new System.Drawing.Size(29, 24);
+			this.btnMostrar.TabIndex = 30;
+			this.btnMostrar.Text = "👁";
+			this.btnMostrar.UseVisualStyleBackColor = true;
+			this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+			// 
+			// FrmPerfil
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.BackgroundImage = global::AcademiaSaude.Properties.Resources.Vector;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnMostrar);
+			this.Controls.Add(this.lblAval);
+			this.Controls.Add(this.txtSen);
+			this.Controls.Add(this.lblSenha);
+			this.Controls.Add(this.txtEml);
+			this.Controls.Add(this.lblEmail);
+			this.Controls.Add(this.pictureBox10);
+			this.Controls.Add(this.txtIdd);
+			this.Controls.Add(this.lblIdd);
+			this.Controls.Add(this.txtSobr);
+			this.Controls.Add(this.lblSbn);
+			this.Controls.Add(this.txtNome);
+			this.Controls.Add(this.lblNm);
+			this.Controls.Add(this.lblEstr5);
+			this.Controls.Add(this.lblEstr4);
+			this.Controls.Add(this.lblEstr3);
+			this.Controls.Add(this.lblEstr2);
+			this.Controls.Add(this.lblEstr1);
+			this.Controls.Add(this.picturePerf);
+			this.Controls.Add(this.pictureBox9);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox8);
+			this.Controls.Add(this.pictureBox7);
+			this.Controls.Add(this.pictureBox6);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox5);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox1);
+			this.DoubleBuffered = true;
+			this.Name = "FrmPerfil";
+			this.Text = "FrmPerfil";
+			this.Load += new System.EventHandler(this.FrmPerfil_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picturePerf)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.PictureBox pictureBox7;
+		private System.Windows.Forms.PictureBox pictureBox8;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox9;
+		private System.Windows.Forms.PictureBox picturePerf;
+		private System.Windows.Forms.Label lblEstr1;
+		private System.Windows.Forms.Label lblEstr2;
+		private System.Windows.Forms.Label lblEstr3;
+		private System.Windows.Forms.Label lblEstr4;
+		private System.Windows.Forms.Label lblEstr5;
+		private System.Windows.Forms.Label lblNm;
+		private System.Windows.Forms.TextBox txtNome;
+		private System.Windows.Forms.Label lblSbn;
+		private System.Windows.Forms.TextBox txtSobr;
+		private System.Windows.Forms.Label lblIdd;
+		private System.Windows.Forms.TextBox txtIdd;
+		private System.Windows.Forms.PictureBox pictureBox10;
+		private System.Windows.Forms.Label lblEmail;
+		private System.Windows.Forms.TextBox txtEml;
+		private System.Windows.Forms.Label lblSenha;
+		private System.Windows.Forms.TextBox txtSen;
+		private System.Windows.Forms.Label lblAval;
+		private System.Windows.Forms.Button btnMostrar;
+	}
+}
---
+++ AcademiaSaude/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("AcademiaSaude")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AcademiaSaude")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("8ae4b35d-272a-4782-9b37-3b9a4f1dda32")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
